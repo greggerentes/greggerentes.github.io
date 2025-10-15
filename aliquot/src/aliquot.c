4af4c5824753fbea6f0e60e2718c4e1f717a65d5
greggerentes <44925920+greggerentes@users.noreply.github.com>
2025-10-15 21:18:13 +0300

    Update aliquot.c

--- aliquot/src/aliquot.c
+++ aliquot/src/aliquot.c
@@ -1,54 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long aliquot_sum(unsigned long long n) {
-    if (n == 1) return 0;
+#define MAX_LIMIT 1000000000000000ULL // 10^15
 
+unsigned long long s(unsigned long long n) {
+    if (n <= 1) return 0;
     unsigned long long sum = 1;
     for (unsigned long long i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             sum += i;
-            unsigned long long j = n / i;
-            if (j != i) sum += j;
+            if (i != n / i)
+                sum += n / i;
         }
     }
     return sum;
 }
 
-int main() {
-    unsigned long long n;
-    unsigned long long max_length;
-    char option;
+int main(void) {
+    unsigned long long n, next;
+    unsigned long long max_len;
+    char mode;
 
-    if (scanf("%llu %llu %c", &n, &max_length, &option) != 3) {
-        fprintf(stderr, "Invalid input\n");
+    printf("Please give the number to start the aliquot sequence from: ");
+    if (scanf("%llu", &n) != 1) {
+        fprintf(stderr, "Input error\n");
         return 1;
     }
 
-    if (n == 0 || (option != 'f' && option != 'l')) {
-        fprintf(stderr, "Invalid input\n");
+    printf("Provide the max aliquot length to look for (0 for unlimited): ");
+    if (scanf("%llu", &max_len) != 1) {
+        fprintf(stderr, "Input error\n");
+        return 1;
+    }
+
+    printf("Do you want to print the full sequence ('f') or just the length ('l')? ");
+    if (scanf(" %c", &mode) != 1 || (mode != 'f' && mode != 'l')) {
+        fprintf(stderr, "Invalid choice\n");
         return 1;
     }
 
     unsigned long long count = 0;
-    while (n != 0 && (max_length == 0 || count < max_length)) {
-        if (n > 1000000000000000ULL) {
-            fprintf(stderr, "Number exceeded 10^15\n");
+    unsigned long long current = n;
+
+    while (1) {
+        if (mode == 'f') {
+            printf("%llu\n", current);
+        }
+
+        if (current == 0) break;
+
+        next = s(current);
+        count++;
+
+        if (next > MAX_LIMIT) {
+            fprintf(stderr, "Number exceeds maximum supported integer (%llu). Stopping.\n", MAX_LIMIT);
             return 1;
         }
 
-        if (option == 'f') {
-            printf("%llu ", n);
-        }
+        if (max_len != 0 && count >= max_len) break;
 
-        n = aliquot_sum(n);
-        count++;
+        current = next;
     }
 
-    if (option == 'l') {
-        printf("%llu\n", count);
-    } else {
-        printf("\n");
+    if (mode == 'l') {
+        printf("Length of aliquot sequence: %llu\n", count);
     }
 
     return 0;
